68bc8bb81288f56ceb6c011067250d9746565cfc
Lee-Young-Jae <ori_@kakao.com>
2024-03-14 21:10:27 +0900

    feat: useForm 커스텀 훅 작성

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+
+type FormValues = {
+  [key: string]: string;
+};
+type FormErrors = {
+  [key: string]: string | undefined;
+};
+type FormTouched = {
+  [key: string]: boolean;
+};
+
+type FormValidateFunction = (value: FormValues) => FormErrors;
+type FormOnSubmitFunction = (value: FormValues) => void;
+
+interface useFormProps {
+  initialState: { [key: string]: string };
+  validate: FormValidateFunction;
+  onSubmit: FormOnSubmitFunction;
+}
+
+const useForm = ({ initialState, validate, onSubmit }: useFormProps) => {
+  const [values, setValues] = useState<FormValues>(initialState);
+  const [errors, setErrors] = useState<FormErrors>({});
+  const [touched, setTouched] = useState<FormTouched>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+
+    const nextValue = {
+      ...values,
+      [name]: value,
+    };
+    setValues(nextValue);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name } = e.target;
+    setTouched((prevTouched) => ({
+      ...prevTouched,
+      [name]: true,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const nextTouched: FormTouched = {};
+    for (const key in values) {
+      nextTouched[key] = true;
+    }
+
+    setTouched(nextTouched);
+
+    const errors = validate(values);
+    setErrors(errors);
+
+    if (Object.values(errors).some(Boolean)) return;
+    onSubmit(values);
+  };
+
+  useEffect(() => {
+    const errors = validate(values);
+    setErrors(errors);
+  }, [values]);
+
+  return {
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  };
+};
+
+export default useForm;
