941ed67f1f4ef5744a5d1dd121499058ff7ecaa3
Lee-Young-Jae <ori_@kakao.com>
2024-03-16 14:56:07 +0900

    style: Mypage 유저인터페이스 작성

---
+++ src/pages/Mypage/Style.tsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  gap: 20px;
+`;
+
+const ImageSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 200px;
+  border-radius: 5px;
+  text-align: center;
+  border: 1px solid #fff;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  input {
+    border: none;
+    background-color: #000;
+    color: #fff;
+    border-bottom: 1px solid #fff;
+    text-align: center;
+    width: 6rem;
+  }
+`;
+
+const Error = styled.p`
+  color: #ff5d5d;
+  font-size: 0.7rem;
+  margin-top: 5px;
+  border-radius: 5px;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: space-between;
+
+  button {
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    background-color: #fff;
+    cursor: pointer;
+    width: 100px;
+  }
+`;
+
+const S = {
+  Container,
+  ImageSection,
+  Buttons,
+  Form,
+  Error,
+};
+
+export default S;
