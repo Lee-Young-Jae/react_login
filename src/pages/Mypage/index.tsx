f4364a1cea935adbb75f917e1740b117979411de
Lee-Young-Jae <ori_@kakao.com>
2024-03-17 00:41:04 +0900

    refact: 유효성 검사 로직 분리

--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import S from "./Style";
 import useForm from "../../hooks/useForm";
+import { validateMyPageForm } from "../../utills/validation/validateMyPageForm";
 
 const MyPage = () => {
   const { userData, isLogin, setIsLogin, mutateUserData } = useAuth();
@@ -13,18 +14,7 @@ const MyPage = () => {
     initialState: {
       name: userData?.name || "",
     },
-    validate: (values: { [key: string]: string }) => {
-      const errors: { [key: string]: string } = {};
-
-      if (!values.name) {
-        errors.name = "빈 이름을 등록할 수 없어요.";
-      }
-
-      if (values.name.length > 5) {
-        errors.name = "이름은 5자 이내로 입력해주세요.";
-      }
-      return errors;
-    },
+    validate: validateMyPageForm,
     onSubmit: () => {
       if (errors.name) {
         alert(errors.name);
