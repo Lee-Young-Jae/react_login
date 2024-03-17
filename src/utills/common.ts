bce235804e215de255e08d55a3e52150b3239401
Lee-Young-Jae <ori_@kakao.com>
2024-03-17 15:37:14 +0900

    refact: 의미 없는 코드를 삭제

--- src/utills/common.ts
+++ src/utills/common.ts
@@ -1,11 +1,7 @@
-const getCookies = (name: string) => {
+export const getCookie = (name: string) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(";").shift();
-};
-
-export const getCookie = (name: string) => {
-  return getCookies(name);
 };
 
 export const setCookie = (name: string, value: string, days: number) => {
